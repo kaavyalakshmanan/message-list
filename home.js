6d2cb1d06ce007834c80b28199da8fd7d97a7630
kaavyalakshmanan <kaavya.laks@gmail.com>
2020-05-13 22:19:35 -0700

    Added basic Add Text functionality

--- home.js
+++ home.js
@@ -2,3 +2,8 @@ function clearField() {
 
     document.getElementById("write-stuff").value = "";
 }
+
+function addTextToList(text){
+    var text = document.getElementById("write-stuff").value;
+    document.getElementById("bottomBox").innerHTML = text;
+}
