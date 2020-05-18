6249cc6ad8e0f36dd1e14e3ca4cceee9fa247075
kaavyalakshmanan <kaavya.laks@gmail.com>
2020-05-18 10:55:48 -0700

    Added show list, hide list, and add to list functionality

--- home.js
+++ home.js
@@ -1,34 +1,44 @@
-var text = {
+var textLoadedToPage = {
     "data": [
-        "Text1",
-        "Text2",
-        "Text3",
-        "Text4"
+        "A short history: Golden’s were first bred from Scotland in the mid 19th century. This breed was developed as a hunting breed. They first came to North America in the 1920’s where they became popular for their excellent temperament and beautiful appearance. In 1925, the AKC (American Kennel Club) registered the golden retriever as a breed.",
+        "The golden retriever is a crossbreed. The breed originates from a Tweed Water Spaniel, which is an extinct breed nowadays, and a Yellow Flat Coated Retriever. This was done back in the 1800’s.",
+        "Golden retrievers have a very high tolerance to pain and can tolerate injuries that many other dogs can’t tolerate. That is part of why they make very good rescuers, police and hunting helpers.",
+        "As the dog ages, his fur will darken. It’s possible to know exactly what colour the newborn pup will have when he is an adult by looking at the colour at the tip of his ears."
     ]
 }
-var stringifiedText = "";
 
 function clearField() {
 
     document.getElementById("write-stuff").value = "";
 }
 
-function addTextToList(text){
-    var text = document.getElementById("write-stuff").value;
-    var li = "<li>" + text + "</li>";
-    document.getElementById("my-list").appendChild(li);
-}
-
-function preLoadedText() {
-    stringifiedText = JSON.stringify(text.data);
-    document.getElementById("bottomBox").onload( loadText);
-}
-
 function loadText() {
+    textLoadedToPage.data.forEach(v => {
+        var node = document.createElement("LI");
+        var textnode = document.createTextNode(v)
+        node.appendChild(textnode);
+        document.getElementById("my-list").appendChild(node);
+})
+}
+
+function hideList() {
+    var x = document.getElementById("my-list");
+      x.style.display = "none";
+}
+
+function showList() {
+    var x = document.getElementById("my-list");
+    x.style.display = "block";
+}
+
+function addTextToList(){
+    var text = document.getElementById("write-stuff").value;
     var node = document.createElement("LI");
-    var textnode = document.createTextNode("Water");
+    var textnode = document.createTextNode(text)
     node.appendChild(textnode);
     document.getElementById("my-list").appendChild(node);
 }
+
+
 
 
